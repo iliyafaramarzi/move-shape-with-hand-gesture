02250b79e319f5bad7ef4f81853ced99b4a58fbe
iliyafaramarzi <iliyafaramazi1384@gmail.com>
2022-04-10 19:27:56 +0430

    version 0.2
    change all the codes and hand detect library and hand gesture for move shapes and now you can move infinite shapes that you want

--- main.py
+++ main.py
@@ -1,44 +1,62 @@
-from this import d
-from traceback import print_tb
+from operator import truediv
+from turtle import distance
 import cv2
-import mediapipe as mp 
-import math
+from cvzone.HandTrackingModule import HandDetector
+from grpc import Status
+from matplotlib.pyplot import draw
+
+
+class DragRect():
+    def __init__(self, Poscenter, size=[100,100]):
+        self.posCenter = Poscenter
+        self.size = size
+        self.is_locked = False
+
+    def update(self, cursor):
+        cx, cy = self.posCenter
+        w, h = self.size
+        if cx-w//2<cursor[0]<cx+w//2 and cy-h//2<cursor[1]<cy+h//2:
+            self.posCenter = cursor
+            self.is_locked = True
+
 
 
 cap = cv2.VideoCapture(0)
-pic = cv2.imread('a.jpg')
 
-mpHands = mp.solutions.hands
-hands = mpHands.Hands()
-mpDraw = mp.solutions.drawing_utils
+detector = HandDetector(detectionCon=0.8)
 
-a, b = 150, 150
-e, f = 250, 150
+rect_list = []
+for i in range(3): # make shape to move 
+    rect_list.append(DragRect([i*130+150,150]))
+
 
 while True:
     success, image = cap.read()
-    cv2.flip(image, 1)
-    rgb_image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-    resualt = hands.process(rgb_image)
-    h , w , c = image.shape
+    image = detector.findHands(image, False)
+    lmList, _  = detector.findPosition(image, draw=False)
 
-    cv2.rectangle(image, (a-50,b-50), (a+50,b+50), (250,250,250), -1)
-    cv2.rectangle(image, (e-50,f-50), (e+50,f+50), (50,50,50), -1)
 
-    if resualt.multi_hand_landmarks:
-        for i in resualt.multi_hand_landmarks:
-            lm = i.landmark
-            cx8, cy8 = int(lm[8].x*w), int(lm[8].y*h)
-            cx4, cy4 = int(lm[4].x*w), int(lm[4].y*h)
-            distance = int(math.hypot(cx8-cx4, cy8-cy4))
+    if lmList:
+        l, _, _ = detector.findDistance(8, 12, image, draw=False)
+        if l<30:
+            cursor = lmList[8]
+            for rect in rect_list:
+                rect.update(cursor) # check hand for move
+                if rect.is_locked == True: # chedck if hand moving a shape then don't move another shape in same time
+                    rect.is_locked = False
+                    break
+            
 
-            if distance <= 20:
-                if a - 50 < cx4 < a + 50 and a - 50 < cx8 < a + 50 and b - 50 < cy4 < b + 50 and b - 50 < cy8 < b + 50:
-                    a = cx4
-                    b = cy4
-                elif e - 50 < cx4 < e + 50 and e - 50 < cx8 < e + 50 and f - 50 < cy4 < f + 50 and f - 50 < cy8 < f + 50:
-                    e = cx4
-                    f = cy4
+    for rect in rect_list: # move shape
+        cx, cy = rect.posCenter
+        w, h = rect.size
+        cv2.rectangle(image, (cx-w//2, cy-h//2), (cx+w//2, cy+h//2), (255,255,255), 2) 
+        
+        
 
     cv2.imshow('main', image)
     cv2.waitKey(1)
+
+
+cap.release()
+cv2.destroyAllWindows()
